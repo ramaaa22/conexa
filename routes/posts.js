82a04c27a7a3cf99c71d152c6058f9c579431781
Ramiro Boza <ramiro.boza@hotmail.com>
2021-12-04 12:34:07 -0300

    changes in route posts

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ let postsController = require('../controllers/posts-controller');
 const validateJwt = require('../middlewares/validate-jwt');
 
 /* GET users listing. */
-router.get('/',validateJwt, postsController.getAll);
+router.get('/',/*validateJwt,*/ postsController.getAll);
 
 
 module.exports = router;
